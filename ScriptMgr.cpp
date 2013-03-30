ec6178b4b088a5446648ed868de2a7378f929d3c
Schmoozerd <schmoozerd@scriptdev2.com>
2013-03-31 00:10:17 +0100

    Revert "Simplify ScriptRegistry::newScript function -- stash later"
    This part is required in case of multiple registered script with one variable.
    Elsewise the second registering will overwrite the stuff of the first and hence the first would not be registered..
    
    This reverts commit 15fa56901fff74d6f165f5f72ea95077b9fce371.

--- ScriptMgr.cpp
+++ ScriptMgr.cpp
@@ -321,6 +321,8 @@ void Script::RegisterSelf(bool bReportError)
 
 Script* ScriptRegistry::newScript(const char* scriptName, bool reportError/*=true*/)
 {
+    Register(); // register last added script (if any)
+
     m_script = new Script(scriptName);
     m_reportError = reportError;
     return m_script;
